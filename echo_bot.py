82e7ef3718a7a5c8079370c5512235a950b29183
Roxxi-Artz <90343355+Roxxi-Artz@users.noreply.github.com>
2022-03-24 11:22:57 -0400

    Add files via upload

---
+++ echo_bot.py
@@ -0,0 +1,6 @@
+echobot = input("Hello! This is an Echobot. It repeats whatever you say to it! Say something: \n")
+
+def stuff_to_echo(user_input):
+    print(f"You just said {user_input}")
+
+stuff_to_echo(echobot)
